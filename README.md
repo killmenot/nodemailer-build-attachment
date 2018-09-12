2826ceb8ff3926ee6cc2fd1166860f40d390d8e9
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2018-09-12 16:44:14 +0300

    refactoring

--- README.md
+++ README.md
@@ -2,17 +2,10 @@
 
 [![Build Status](https://travis-ci.org/killmenot/nodemailer-build-attachment.svg?branch=master)](https://travis-ci.org/killmenot/nodemailer-build-attachment)
 [![Coverage Status](https://coveralls.io/repos/github/killmenot/nodemailer-build-attachment/badge.svg?branch=master)](https://coveralls.io/github/killmenot/nodemailer-build-attachment?branch=master)
-[![Dependency Status](https://gemnasium.com/badges/github.com/killmenot/nodemailer-build-attachment.svg)](https://gemnasium.com/github.com/killmenot/nodemailer-build-attachment)
+[![Dependency Status](https://david-dm.org/killmenot/nodemailer-build-attachment.svg)](https://david-dm.org/killmenot/nodemailer-build-attachment.svg)
 [![npm version](https://badge.fury.io/js/nodemailer-build-attachment.svg)](https://badge.fury.io/js/nodemailer-build-attachment)
 
-Build and stream attachment's content.
-
-Based on [Andris Reinman](https://github.com/andris9)'s work in [buildmail](https://github.com/nodemailer/buildmail).
-
-## Requirements
-
- - Node.js 4+
-
+Build and stream attachment's content. Based on [Andris Reinman](https://github.com/andris9)'s work in [buildmail](https://github.com/nodemailer/buildmail).
 
 ## Install
 
@@ -26,14 +19,14 @@ npm install nodemailer-build-attachment
 ```javascript
 'use strict';
 
-var BuildAttachment = require('nodemailer-build-attachment');
-var fs = require('fs');
-var stream = fs.createReadStream('path/to/file.txt');
-var options = {
+const BuildAttachment = require('nodemailer-build-attachment');
+const fs = require('fs');
+const stream = fs.createReadStream('path/to/file.txt');
+const options = {
     contentTransferEncoding: 'base64'
 };
 
-new BuildAttachment(options).setContent(stream).build(function (err, attachment) {
+new BuildAttachment(options).setContent(stream).build((err, attachment) => {
     console.log(attachment.content.toString()); // base64 encoded string
 });
 
@@ -42,22 +35,22 @@ new BuildAttachment(options).setContent(stream).build(function (err, attachment)
 
 ## Licence
 
-The MIT License (MIT)
+    The MIT License (MIT)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
--- test/build-attachment.test.js
+++ test/build-attachment.test.js
@@ -1,35 +1,30 @@
-/* globals describe, it */
-
 'use strict';
 
-var expect = require('chai').expect;
-var fs = require('fs');
-var path = require('path');
-var BuildAttachment = require('../');
+const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+const BuildAttachment = require('../');
 
-describe('BuildAttachment', function () {
-  var buildAttachment;
-  var filename = path.join(__dirname, 'fixtures', 'test.txt');
-  var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n';
+const filename = path.join(__dirname, 'fixtures', 'test.txt');
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n';
 
-  it('should be defined', function () {
-    expect(BuildAttachment).to.be.a('function');
-  });
+describe('BuildAttachment', () => {
+  let buildAttachment;
 
-  describe('BuildAttachment()', function () {
-    it('with default options', function () {
+  describe('BuildAttachment()', () => {
+    it('with default options', () => {
       buildAttachment = new BuildAttachment();
 
-      expect(buildAttachment.contentTransferEncoding).to.equal('');
-      expect(buildAttachment.contentDisposition).to.equal('attachment');
-      expect(buildAttachment.filename).to.equal('');
-      expect(buildAttachment.contentType).to.equal('');
-      expect(buildAttachment.encoding).to.equal('');
-      expect(buildAttachment.cid).to.equal('');
-      expect(buildAttachment.headers).to.eql([]);
+      expect(buildAttachment.contentTransferEncoding).equal('');
+      expect(buildAttachment.contentDisposition).equal('attachment');
+      expect(buildAttachment.filename).equal('');
+      expect(buildAttachment.contentType).equal('');
+      expect(buildAttachment.encoding).equal('');
+      expect(buildAttachment.cid).equal('');
+      expect(buildAttachment.headers).eql([]);
     });
 
-    it('with custom options', function () {
+    it('with custom options', () => {
       buildAttachment = new BuildAttachment({
         contentTransferEncoding: 'base64',
         contentDisposition: 'attachment-1',
@@ -45,13 +40,13 @@ describe('BuildAttachment', function () {
         ]
       });
 
-      expect(buildAttachment.contentTransferEncoding).to.equal('base64');
-      expect(buildAttachment.contentDisposition).to.equal('attachment-1');
-      expect(buildAttachment.filename).to.equal('text.txt');
-      expect(buildAttachment.contentType).to.equal('text/plain');
-      expect(buildAttachment.encoding).to.equal('utf-8');
-      expect(buildAttachment.cid).to.equal('cid:foo@bar.net');
-      expect(buildAttachment.headers).to.eql([
+      expect(buildAttachment.contentTransferEncoding).equal('base64');
+      expect(buildAttachment.contentDisposition).equal('attachment-1');
+      expect(buildAttachment.filename).equal('text.txt');
+      expect(buildAttachment.contentType).equal('text/plain');
+      expect(buildAttachment.encoding).equal('utf-8');
+      expect(buildAttachment.cid).equal('cid:foo@bar.net');
+      expect(buildAttachment.headers).eql([
         {
           key: 'X-Key-Name',
           value: 'val1'
@@ -59,74 +54,74 @@ describe('BuildAttachment', function () {
       ]);
     });
 
-    it('with filename', function () {
+    it('with filename', () => {
       buildAttachment = new BuildAttachment({
         filename: 'page.html',
       });
-      expect(buildAttachment.contentType).to.equal('text/html');
+      expect(buildAttachment.contentType).equal('text/html');
     });
   });
 
-  it('should return instance to itself', function (done) {
+  it('should return instance to itself', (done) => {
     buildAttachment = new BuildAttachment();
 
-    buildAttachment.setContent('hello world').build(function (err, attachment) {
-      expect(attachment).to.equal(buildAttachment);
+    buildAttachment.setContent('hello world').build((err, attachment) => {
+      expect(attachment).equal(buildAttachment);
       done();
     });
   });
 
-  it('should build string content', function (done) {
-    new BuildAttachment().setContent('hello world').build(function (err, attachment) {
-      expect(attachment.content.toString()).to.equal('hello world');
+  it('should build string content', (done) => {
+    new BuildAttachment().setContent('hello world').build((err, attachment) => {
+      expect(attachment.content.toString()).equal('hello world');
       done();
     });
   });
 
-  it('should build stream content', function (done) {
+  it('should build stream content', (done) => {
     var content = fs.createReadStream(filename);
-    new BuildAttachment().setContent(content).build(function (err, attachment) {
-      expect(attachment.content.toString()).to.be.eql(text);
+    new BuildAttachment().setContent(content).build((err, attachment) => {
+      expect(attachment.content.toString()).be.eql(text);
       done();
     });
   });
 
-  it('should build path content', function (done) {
+  it('should build path content', (done) => {
     new BuildAttachment().setContent({
       path: filename
-    }).build(function (err, attachment) {
-      expect(attachment.content.toString()).to.be.eql(text);
+    }).build((err, attachment) => {
+      expect(attachment.content.toString()).be.eql(text);
       done();
     });
   });
 
-  it('should build href content', function (done) {
+  it('should build href content', (done) => {
     new BuildAttachment().setContent({
       href: 'https://raw.githubusercontent.com/killmenot/nodemailer-build-attachment/master/test/fixtures/test.txt'
-    }).build(function (err, attachment) {
-      expect(attachment.content.toString()).to.be.eql(text);
+    }).build((err, attachment) => {
+      expect(attachment.content.toString()).be.eql(text);
       done();
     });
   });
 
-  it('should build content to base64 string', function (done) {
+  it('should build content to base64 string', (done) => {
     new BuildAttachment({
       contentTransferEncoding: 'base64'
-    }).setContent('hello world').build(function (err, attachment) {
-      expect(attachment.content.toString()).to.equal('aGVsbG8gd29ybGQ=');
+    }).setContent('hello world').build((err, attachment) => {
+      expect(attachment.content.toString()).equal('aGVsbG8gd29ybGQ=');
       done();
     });
   });
 
-  it('should build content with passed lineLength', function (done) {
+  it('should build content with passed lineLength', (done) => {
     var options = {
       lineLength: 10
     };
     
     new BuildAttachment({
       contentTransferEncoding: 'base64'
-    }).setContent('Lorem ipsum dolor sit amet').build(options, function (err, attachment) {
-      expect(attachment.content.toString()).to.equal('TG9yZW0gaX\r\nBzdW0gZG9s\r\nb3Igc2l0IG\r\nFtZXQ=');
+    }).setContent('Lorem ipsum dolor sit amet').build(options, (err, attachment) => {
+      expect(attachment.content.toString()).equal('TG9yZW0gaX\r\nBzdW0gZG9s\r\nb3Igc2l0IG\r\nFtZXQ=');
       done();
     });
   });
